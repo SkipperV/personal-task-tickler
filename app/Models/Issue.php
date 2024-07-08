39914f12c34c239a5b65b12dba9d8598dc13e4f6
SkipperV <vlad.tymchuk@gmail.com>
2024-07-08 15:26:58 +0300

    Issue relations

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Issue extends Model
 {
@@ -38,5 +39,37 @@ class Issue extends Model
     public function getCodeAttribute(): string
     {
         return $this->space->code . "-" . $this->id_within_space;
+    }
+
+    public function blockedBy(): BelongsToMany|bool
+    {
+        return $this->belongsToMany(Issue::class, 'issue_relations', 'child_issue_id', 'parent_issue_id')
+            ->withPivot('relationship_type')
+            ->wherePivot('relationship_type', 'Blocked by')
+            ?? false;
+    }
+
+    public function isBlocking(): BelongsToMany|bool
+    {
+        return $this->belongsToMany(Issue::class, 'issue_relations', 'parent_issue_id', 'child_issue_id')
+            ->withPivot('relationship_type')
+            ->wherePivot('relationship_type', 'Blocked by')
+            ?? false;
+    }
+
+    public function segmentedBy(): BelongsToMany|bool
+    {
+        return $this->belongsToMany(Issue::class, 'issue_relations', 'child_issue_id', 'parent_issue_id')
+            ->withPivot('relationship_type')
+            ->wherePivot('relationship_type', 'Segmented by')
+            ?? false;
+    }
+
+    public function segmentOf(): BelongsToMany|bool
+    {
+        return $this->belongsToMany(Issue::class, 'issue_relations', 'parent_issue_id', 'child_issue_id')
+            ->withPivot('relationship_type')
+            ->wherePivot('relationship_type', 'Segment of')
+            ?? false;
     }
 }
---
+++ database/migrations/2024_07_07_145947_create_issue_relations_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Issue;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('issue_relations', function (Blueprint $table) {
+            $table->id();
+//            $table->foreignIdFor(Issue::class, 'parent_issue_id')->constrained()->cascadeOnDelete();
+//            $table->foreignIdFor(Issue::class, 'child_issue_id')->constrained()->cascadeOnDelete();
+            $table->unsignedBigInteger('parent_issue_id');
+            $table->unsignedBigInteger('child_issue_id');
+            $table->set('type', ['Blocked by', 'Segmented by']);
+            $table->timestamps();
+
+            $table->foreign('parent_issue_id')
+                ->references('id')
+                ->on('issues')
+                ->cascadeOnDelete();
+
+            $table->foreign('child_issue_id')
+                ->references('id')
+                ->on('issues')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('issue_relations');
+    }
+};
