3a38f8f75b2967e19e2fa094ff0232ec0dd63f8b
SkipperV <vlad.tymchuk@gmail.com>
2024-10-01 23:11:14 +0300

    SPA Auth

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,11 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+axios.defaults.withCredentials = true;
+
+axios.get('/sanctum/csrf-cookie').then(response => {
+    console.log('CSRF token set');
+}).catch(error => {
+    console.error('Error initializing CSRF protection:', error);
+});
