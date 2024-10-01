d1d912234b2883bec1bdaf01d4c6a4bcdb193933
SkipperV <vlad.tymchuk@gmail.com>
2024-10-01 23:11:40 +0300

    First set of routes (Spaces, Homepage)

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\SpaceController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -22,6 +15,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/', [HomepageController::class, 'index'])->name('home.index');
+
+    Route::get('/spaces', [SpaceController::class, 'index'])->name('spaces.index');
+
+    Route::get('/spaces/{space}/all', [SpaceController::class, 'show'])->name('spaces.show');
+    Route::get('/spaces/{space}/archived', [SpaceController::class, 'show'])->name('spaces.tasks.archived');
 });
 
-require __DIR__.'/auth.php';
+//Admin (Currently for testing purposes)
+Route::middleware('auth')->group(function () {
+    Route::get('/tasks/all', [TaskController::class, 'index'])->name('tasks.index');
+});
+
+require __DIR__ . '/auth.php';
