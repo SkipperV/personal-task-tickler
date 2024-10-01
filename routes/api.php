d9efdc9cb62d2c25caf806d056b7965dac0ee0d8
SkipperV <vlad.tymchuk@gmail.com>
2024-10-01 23:10:55 +0300

    API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\SpaceController;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::name('api.')->group(function () {
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+        Route::get('/spaces', [SpaceController::class, 'index'])->name('spaces.index');
+        Route::get('/spaces/{space}', [SpaceController::class, 'show'])->name('spaces.show');
+
+        Route::get('/spaces/{space}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+        Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+        Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    });
+});
