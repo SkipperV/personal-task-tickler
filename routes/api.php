95654b56fab91363e5599e16b979855564dce35f
SkipperV <vlad.tymchuk@gmail.com>
2024-12-09 10:33:38 +0200

    test: temp API routes for testing

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\SpaceController;
 use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\TaskStatusController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,9 +14,14 @@ Route::name('api.')->group(function () {
 
         Route::get('/spaces', [SpaceController::class, 'index'])->name('spaces.index');
         Route::get('/spaces/{space}', [SpaceController::class, 'show'])->name('spaces.show');
+        Route::get('/spaces/{space}/tasks', [SpaceController::class, 'tasks'])->name('spaces.tasks.index');
 
-        Route::get('/spaces/{space}/tasks', [TaskController::class, 'index'])->name('tasks.index');
         Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
         Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+        // Currently for testing
+        Route::get('spaces/{space}/task-statuses', [TaskStatusController::class, 'index'])->name('spaces.statuses.index');
+        Route::get('spaces/{space}/task-statuses/{taskStatus}', [TaskStatusController::class, 'show'])->name('spaces.statuses.show');
+        Route::get('/tasks/all', [TaskController::class, 'index'])->name('tasks.index');
     });
 });
